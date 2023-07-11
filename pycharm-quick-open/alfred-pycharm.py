674e838d53c99ed7fa831964246a75e904f71b9c
linshuai <nsity223@gmail.com>
2023-07-11 14:42:31 +0800

    update: regex

--- pycharm-quick-open/alfred-pycharm.py
+++ pycharm-quick-open/alfred-pycharm.py
@@ -10,48 +10,47 @@ RECENT_XPATH = ".//component[@name='RecentProjectsManager']/option[@name='additi
 
 
 def parse_start_script(path=LAUNCHER_DIR):
-    run_path, config_path = None, None
+    config_path = None
     for line in open(path, "r"):
         line = line.strip()
         if line.startswith("CONFIG_PATH = "):
             config_path = line.split("=")[1].strip().replace("u'", "").rstrip("'")
-        elif line.startswith("RUN_PATH = "):
-            run_path = line.split("=")[1].strip().replace("u'", "").rstrip("'")
-    return run_path, config_path
+    return config_path
 
 
 def match_name(path_with_names, query):
-    fuzzy_re = ".*".join(query)
+    fuzzy_re = re.compile(".*".join(query), re.IGNORECASE)
     for path, name in path_with_names:
-        if re.match(fuzzy_re, name, re.IGNORECASE):
+        if fuzzy_re.match(name):
             yield path, name
 
 
 def main():
-    pycharm_path, config_path = parse_start_script()
-    home_dir = os.path.expanduser("~")
-
-    root = ET.parse(config_path + "/options/recentProjects.xml")
-    project_paths = (
-        el.attrib["key"].replace("$USER_HOME$", home_dir)
-        for el in root.findall(RECENT_XPATH)
-    )
-
-    path_with_names = ((path, os.path.basename(path)) for path in project_paths)
-
-    query = sys.argv[1]
+    config_path = parse_start_script()
     alfred_results = []
-    for path, name in match_name(path_with_names, query):
-        alfred_results.append(
-            {
-                "title": name,
-                "subtitle": path,
-                "arg": path,
-                "uid": path,
-                "autocomplete": path,
-                "icon": {"path": "./icon.png"},
-            }
+    if config_path:
+        home_dir = os.path.expanduser("~")
+
+        root = ET.parse(config_path + "/options/recentProjects.xml")
+        project_paths = (
+            el.attrib["key"].replace("$USER_HOME$", home_dir)
+            for el in root.findall(RECENT_XPATH)
         )
+
+        path_with_names = ((path, os.path.basename(path)) for path in project_paths)
+
+        query = sys.argv[1]
+        for path, name in match_name(path_with_names, query):
+            alfred_results.append(
+                {
+                    "title": name,
+                    "subtitle": path,
+                    "arg": path,
+                    "uid": path,
+                    "autocomplete": path,
+                    "icon": {"path": "./icon.png"},
+                }
+            )
     response = json.dumps({"items": alfred_results})
     sys.stdout.write(response)
 
